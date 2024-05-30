58771bca042a70f8d8090a7488360569eb368c92
IndraHfid <indrahafidsaputra@gmail.com>
2024-05-30 15:00:18 +0700

    menambahkan pengecekan overflow dan kosong pada antrean melingkar dengan pembaruan pointer belakang

--- CircularQueuesArray_0222/CircularQueuesArray_0222.cpp
+++ CircularQueuesArray_0222/CircularQueuesArray_0222.cpp
@@ -23,6 +23,26 @@ public:
         cin >> num;
         cout << endl;
 
+        //cek apakah antrean penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+        //cek apakah antrean kosong
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        }
+
+        else {
+            //jika REAR berada diposisi terahir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
 };
 
