2fc8955184738ee4bab338bc4e0288559bb6d8ac
IndraHfid <indrahafidsaputra@gmail.com>
2024-05-30 15:08:34 +0700

    Implementasi penghapusan elemen antrian

--- CircularQueuesArray_0222/CircularQueuesArray_0222.cpp
+++ CircularQueuesArray_0222/CircularQueuesArray_0222.cpp
@@ -44,6 +44,30 @@ public:
         }
         queue_array[REAR] = num;
     }
+
+    void remove() {
+        //cek apakah antrean kosong 
+        if (FRONT == -1) {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        //cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
+
+        else {
+            //jika elemen yang dihapus berada diposisi terahir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+
+        }
+    }
 };
 
 int main()
